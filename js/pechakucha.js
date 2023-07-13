b67e8a8b11eb7570b9668d4f978b493d19e33968
Julien Bidoret <julien@accentgrave.net>
2023-07-13 12:51:04 +0200

    Adjusted javascript with grid and actions

--- js/pechakucha.js
+++ js/pechakucha.js
@@ -1,120 +1,160 @@
+// ぺちゃくちゃ
 
-	let idx = 0;
+const articles = document.querySelectorAll('article');
+const bar = document.getElementById('bar');
+let timer = null,
+	displaygrid = false,
+	basesize = 4,
+	idx = 0,
+	has_started = false;
 
-	const articles = document.querySelectorAll('article');
-	const bar = document.getElementById('bar');
-	let timer = null;
+// affichage du premier article
+articles[0].classList.add('visible');
 
-	// affichage du premier article
-	articles[0].classList.add('visible');
+// attribution d’un id et d’un attribut “data-id” à chaque article
+articles.forEach(function(article, index){
+	article.id = "article-" + index;
+	article.dataset.id = index;
+	initGridActions(article);
+})
 
-	// attribution d’un id à chaque article
-	articles.forEach(function(el, index, array){
-		el.id = "article-" + index;
+
+// navigation au clavier (flèches directionelles)
+document.body.onkeydown = function(e){
+
+	if (e.key == "ArrowLeft") changeSlide('left');
+	if (e.key == "ArrowRight") changeSlide('right');
+	if (e.key == "Enter") start();
+	if (e.key == "Escape") toggleGrid();      
+	
+	if (e.key == "+") {
+		basesize += .1;
+		document.body.style.setProperty('--basesize', basesize + "vw")
+	}
+	if (e.key == "-") {
+		basesize -= .1;
+		document.body.style.setProperty('--basesize', basesize + "vw")
+	}
+};
+
+
+// rafraichissement de page
+if(window.location.hash) {
+	let myidx = window.location.hash.replace("#article-", '')
+	let article = document.querySelector(window.location.hash)
+	idx = Array.prototype.indexOf.call(articles, article) - 1;
+	changeSlide('right');
+}
+
+
+function start(){
+	if(!has_started){
+		toggleFullScreen();		
+	}
+	changeSlide('right');
+}
+
+// toggle grid
+function toggleGrid() {
+	let body = document.body;
+	if (!displaygrid) {
+		body.classList.add('grid')
+		displaygrid = true;
+	} else {
+		body.classList.remove('grid')
+		displaygrid = false;
+	}
+}
+
+// grid actions
+function initGridActions(article){
+	// en mode grille, au clic sur un article, quitte le mode grille et affiche cet article  
+	article.addEventListener('click', () => {
+		if(displaygrid == true) {
+			document.querySelector('.visible').classList.remove("visible");
+			toggleGrid();
+			article.classList.add('visible');
+			idx = parseInt(article.dataset.id) - 1;
+			changeSlide("right");
+		}
 	})
+}
 
+// Full screen
+document.body.ondblclick = function (e) {
+	toggleFullScreen();
+};
 
-	// navigation au clavier (flèches directionelles)
-	document.body.onkeydown = function(e){
-		if(e.keyCode == 37 || e.keyCode == 39 || e.keyCode == 13) e.preventDefault();
-		if (e.keyCode == 37) changeSlide('left');
-	    if (e.keyCode == 39) changeSlide('right');
-	    if (e.keyCode == 13) {
-	    	start();
-	    	// toggleFullScreen();
-	    }
-	};
-
-
-	// rafraichissement de page
-	if(window.location.hash) {
-		var myidx = window.location.hash.replace("#article-", '')
-		var el = document.querySelector(window.location.hash)
-		idx = Array.prototype.indexOf.call(articles, el) - 1;
-		changeSlide('right');
+function toggleFullScreen() {
+	if (!document.fullscreenElement) {
+		document.documentElement.requestFullscreen();
+	} else {
+		if (document.exitFullscreen) {
+				document.exitFullscreen();
+		}
 	}
+}
 
+// changement de slide
+function changeSlide(direction){
+	
+		// quelle direction ?
+		if (direction == 'right') {
+			idx = idx == articles.length - 1 ? 0 : idx + 1;
+		} else {
+			idx = idx == 0 ? articles.length - 1 : idx - 1;
+		}
 
-	function start(){
-		changeSlide('right');
-	}
-
-	function toggleFullScreen() {
-  	if (!document.fullscreenElement) {
-    	document.documentElement.requestFullscreen();
-  	} else {
-    	if (document.exitFullscreen) {
-      		document.exitFullscreen();
-    	}
-  	}
-	}
-
-	// changement de slide
-	function changeSlide(direction){
-
-    // console.log('changeSlide: ' + direction);
-
-    clearTimeout(timer);
-    timer = setTimeout(function(){
-        changeSlide('right');
-    }, 20000)
-
-		// animation
+		// restart timer animation
 		bar.classList.remove('animated');
 		void bar.offsetWidth;
-		bar.classList.add('animated');
+		if(idx != 0){
+			bar.classList.add('animated');
+		}
 
-	  	// quelle direction ?
-	  	if (direction == 'right') {
-	  		idx = idx == articles.length - 1 ? 0 : idx + 1;
-	  	} else {
-	  		idx = idx == 0 ? articles.length - 1 : idx - 1;
-	  	}
+		// check each article
+		articles.forEach(function(el, index, array){
 
-      // console.log('index: ' + idx);
+			// Si c’est la slide qu’on veut afficher
+			if (index == idx) {
+				el.classList.add('visible');
 
-	  	articles.forEach(function(el, index, array){
+				// change le “hash” dans l’URL
+				history.pushState(null, el.id, '#' + el.id);
 
-	  		// Si c’est la slide qu’on veut afficher
-	  		if (index == idx) {
-	  			el.classList.add('visible');
+				// auto build iframes
+				let embed = el.querySelectorAll('.embed')[0] || null
+				if (embed !== null) {
+					let iframe = document.createElement('iframe');
+					iframe.src = embed.getAttribute('rel');
+					iframe.setAttribute('width', 854);
+					iframe.setAttribute('autoplay', 'true');
+					iframe.setAttribute('height', 480);
+					iframe.setAttribute('frameborder', 0);
+					embed.appendChild(iframe);
+					embed.className = 'embedded';
+				}
 
-	  			// change le “hash” dans l’URL
-	  			history.pushState(null, el.id, '#' + el.id);
+				// autoplay videos
+				let video = el.querySelectorAll('video')[0] || null;
+				if (video) video.play();
+			}
+			// Sinon…
+			else {
+				el.classList.remove('visible');
 
-	  			// auto build iframes
-	  			let embed = el.querySelectorAll('.embed')[0] || null
-	  			if (embed !== null) {
-	  				let iframe = document.createElement('iframe');
-	  				iframe.src = embed.getAttribute('rel');
-	  				iframe.setAttribute('width', 854);
-	  				iframe.setAttribute('autoplay', 'true');
-	  				iframe.setAttribute('height', 480);
-	  				iframe.setAttribute('frameborder', 0);
-	  				embed.appendChild(iframe);
-	  				embed.className = 'embedded';
-	  			}
+				// auto destroy iframes
+				let embedded = el.querySelectorAll('.embedded')[0] || null
+				if (embedded !== null) {
+					let iframe = embedded.querySelectorAll('iframe')[0];
+					embedded.setAttribute('rel', iframe.src);
+					embedded.removeChild(iframe);
+					embedded.className = 'embed';
+				}
 
-	  			// autoplay videos
-	  			let video = el.querySelectorAll('video')[0] || null;
-	  			if (video) video.play();
-	  		}
-	  		// Sinon
-	  		else {
-	  			el.classList.remove('visible');
-
-	  			// auto destroy iframes
-	  			let embedded = el.querySelectorAll('.embedded')[0] || null
-	  			if (embedded !== null) {
-	  				let iframe = embedded.querySelectorAll('iframe')[0];
-	  				embedded.setAttribute('rel', iframe.src);
-	  				embedded.removeChild(iframe);
-	  				embedded.className = 'embed';
-	  			}
-
-	  			// pause videos
-	  			let video = el.querySelectorAll('video')[0] || null;
-	  			if (video) video.pause();
-	  		}
-	  	})
-	}
+				// pause videos
+				let video = el.querySelectorAll('video')[0] || null;
+				if (video) video.pause();
+			}
+		})
+}
